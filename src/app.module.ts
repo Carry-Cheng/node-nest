0d7a86b495433c1e0ad167474dfe79c4a3b5d801
周成 <1057415350@qq.com>
2019-07-30 14:12:17 +0800

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatModule } from './module/cat/cat.module';
 
 @Module({
-  imports: [],
+  imports: [CatModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
--- src/module/cat/cat.controller.ts
+++ src/module/cat/cat.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Req, HttpCode, Header } from '@nestjs/common';
+import { Request } from 'express';
 
 @Controller('cat')
-export class CatController {}
+export class CatController {
+
+  @Get()
+  find(@Req() request: Request): any {
+    return 'cat'
+  }
+
+  @Get('hello')
+  search(): any {
+    let v: any = {}
+    v.name = 'zhou'
+    v.age = 18
+    return v
+  }
+
+  @Post('create')
+  @Header('Cache-Control', 'none')
+  create(): void {
+    console.info('post')
+  }
+
+}
